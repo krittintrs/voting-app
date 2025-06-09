8fca28246ca41780c6b79cfd97168aca2d4787d0
Krittin Thirasak <krittin.thi@quantium.pe>
2025-06-09 10:29:02 +0700

    feat: display list of votes in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VoteService } from 'src/app/services/vote.service';
+import { Vote } from '../models/vote.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  votes: Vote[] = [];
 
+  constructor(private VoteService: VoteService) {}
+
+  ngOnInit(): void {
+    this.VoteService.getVotes().subscribe((data: Vote[]) => {
+      this.votes = data;
+    });
+  }
+
+  openModal(vote: Vote, action: string): void {}
 }
